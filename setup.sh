0a43e6f98e5c38315fbb3a227859a7ffd897fe1c
fhightower <floyd.hightower27@gmail.com>
2017-05-13 20:06:33 -0300

    Creating db folder to make things easier/cleaner

--- setup.sh
+++ setup.sh
@@ -10,16 +10,16 @@ sudo pip install eve
 sudo pip install redis
 
 # clone rest API foundation
-git clone https://github.com/agenoria/foundation-rest-api-framework.git;
-cd foundation-rest-api-framework;
+git clone https://github.com/agenoria/eve-rest-api-template.git;
+cd eve-rest-api-template;
 
 # minor bug fix for systems set to local locale
 vi ~/.bashrc;
 # paste the following into .bashrc: export LC_ALL=C
 
 # run mongo using small files and as a daemon (--fork)
-mkdir logs/ && vi logs/mongo_log.log;
-mongod --dbpath . --smallfiles --port 27018 --fork --logpath ./logs/mongo_log.log;
+mkdir db/ && mkdir db/logs && touch db/logs/mongo_log.log;
+mongod --dbpath ./db --smallfiles --port 27018 --fork --logpath ./db/logs/mongo_log.log;
 
 # run redis
 redis-server;
@@ -28,4 +28,4 @@ redis-server;
 python run.py
 
 # you can stop mongo with:
-#  mongod --shutdown --dbpath .
+#  mongod --shutdown --dbpath ./db
